d30c7ea6b434df716b0b9e947271c6c9f230f397
Jongseung Lim <penandlim@gmail.com>
2023-07-12 00:09:12 -0400

    chore: Install "evmfetch" binary

--- evmfetch-rs.rb
+++ evmfetch-rs.rb
@@ -4,10 +4,8 @@ class EvmfetchRs < Formula
     url "https://github.com/your-username/evmfetch-rs/releases/download/v0.1.0/evmfetch-rs.tar.gz"
     sha256 "0977509c5e6219c718018fc067a384a485afda6271dc32352e3ba0afa58743a3"
   
-    depends_on "rust" => :build
-
     def install
-      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+        bin.install "evmfetch"  # Assuming "evmfetch" is the name of your binary file
     end
   end
   
