c108846414ca68019a2f1617e581cdc5ce257a2a
Jongseung Lim <penandlim@gmail.com>
2023-07-12 00:16:59 -0400

    chore: Update evmfetch-rs to version 0.1.1

--- evmfetch-rs.rb
+++ evmfetch-rs.rb
@@ -1,8 +1,9 @@
 class EvmfetchRs < Formula
     desc "Fetches contract ABI and source code using the Etherscan API"
     homepage "https://github.com/penandlim/evmfetch-rs"
-    url "https://github.com/penandlim/evmfetch-rs/releases/download/v0.1.0/evmfetch-rs.tar.gz"
-    sha256 "0977509c5e6219c718018fc067a384a485afda6271dc32352e3ba0afa58743a3"
+    url "https://github.com/penandlim/evmfetch-rs/releases/download/v0.1.1/evmfetch-rs.tar.gz"
+    sha256 "1267cf544e890c10880bb220f94657ccc6dc9a08f9dc3510a8608887aa327135"
+    version "0.1.1"
   
     def install
         bin.install "evmfetch"  # Assuming "evmfetch" is the name of your binary file
