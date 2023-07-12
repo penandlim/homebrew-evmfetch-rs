e07b826bd1179d08236371868bf49ee49ae90bd5
Jongseung Lim <penandlim@gmail.com>
2023-07-12 00:09:52 -0400

    fix: Fix URL in EvmfetchRs formula

--- evmfetch-rs.rb
+++ evmfetch-rs.rb
@@ -1,7 +1,7 @@
 class EvmfetchRs < Formula
     desc "Fetches contract ABI and source code using the Etherscan API"
     homepage "https://github.com/penandlim/evmfetch-rs"
-    url "https://github.com/your-username/evmfetch-rs/releases/download/v0.1.0/evmfetch-rs.tar.gz"
+    url "https://github.com/penandlim/evmfetch-rs/releases/download/v0.1.0/evmfetch-rs.tar.gz"
     sha256 "0977509c5e6219c718018fc067a384a485afda6271dc32352e3ba0afa58743a3"
   
     def install
