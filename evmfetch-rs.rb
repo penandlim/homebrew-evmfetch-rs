a06478f58d591f0b629b60dc87172745694a01c7
Jongseung Lim <penandlim@gmail.com>
2023-07-12 00:20:06 -0400

    chore(evmfetch-rs.rb): update sha256 hash for v0.1.1 release

--- evmfetch-rs.rb
+++ evmfetch-rs.rb
@@ -2,7 +2,7 @@ class EvmfetchRs < Formula
     desc "Fetches contract ABI and source code using the Etherscan API"
     homepage "https://github.com/penandlim/evmfetch-rs"
     url "https://github.com/penandlim/evmfetch-rs/releases/download/v0.1.1/evmfetch-rs.tar.gz"
-    sha256 "1267cf544e890c10880bb220f94657ccc6dc9a08f9dc3510a8608887aa327135"
+    sha256 "f8943705369ac986df367afd6b9f47e9d3826a4b2fa680c01bb1346f91c7e8c0"
     version "0.1.1"
   
     def install
