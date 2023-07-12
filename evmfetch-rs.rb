3cb4c027bf23a067d6e5bfc99cdbac644dbbac9e
Jongseung Lim <penandlim@gmail.com>
2023-07-12 00:04:05 -0400

    chore: Update sha256 in evmfetch-rs.rb

--- evmfetch-rs.rb
+++ evmfetch-rs.rb
@@ -2,7 +2,7 @@ class EvmfetchRs < Formula
     desc "Fetches contract ABI and source code using the Etherscan API"
     homepage "https://github.com/penandlim/evmfetch-rs"
     url "https://github.com/your-username/evmfetch-rs/releases/download/v0.1.0/evmfetch-rs.tar.gz"
-    sha256 "1611fc0c589c6d02bb2658fcb3ca694a8eb0b9d566f3e8664c64ba303c247518"
+    sha256 "0977509c5e6219c718018fc067a384a485afda6271dc32352e3ba0afa58743a3"
   
     depends_on "rust" => :build
 
